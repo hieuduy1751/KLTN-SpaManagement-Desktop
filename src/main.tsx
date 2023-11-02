f10fa7a1f0fcd9787771aca84791d89a38e557f9
hieuduy1751 <hieuduy1751@gmail.com>
2023-11-02 22:41:26 +0700

    update app tsx

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./firebase";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// Remove Preload scripts loading
+postMessage({ payload: "removeLoading" }, "*");
+
+// Use contextBridge
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
