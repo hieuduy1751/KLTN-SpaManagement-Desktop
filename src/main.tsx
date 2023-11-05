626b880832169a5d780bde886f4e01b9a59b8092
hieuduy1751 <hieuduy1751@gmail.com>
2023-11-05 22:41:26 +0700

    update based files

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./firebase";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // Remove Preload scripts loading
