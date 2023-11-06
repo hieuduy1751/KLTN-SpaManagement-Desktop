6afd1c977dae1450459b9f26f4a4d63e989d8ad6
hieuduy1751 <hieuduy1751@gmail.com>
2023-11-06 22:41:26 +0700

    add app router

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,15 +1,49 @@
-import Sidebar from '../components/Sidebar'
-import { Outlet } from 'react-router-dom'
+import { Breadcrumb } from "antd";
+import Sidebar from "../components/Sidebar";
+import { Outlet, useLocation } from "react-router-dom";
+import { HomeOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import PATH from "../constants/path";
 
 export default function MainLayout() {
+  const location = useLocation();
+  const [items, setItems] = useState<any>([]);
+
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      const paths = location.pathname.split("/");
+      const newItems: any = [];
+      paths.forEach((path: string) => {
+        newItems.push({
+          href: "",
+          title: PATH.find((p) => p.path === path)?.name,
+        });
+      });
+      setItems(newItems);
+    }
+  }, [location.pathname]);
+
   return (
-    <div className='w-full h-[100vh] flex'>
-      <div className='w-[15%] 2xl:w-[10%] h-full bg-gray-200'>
+    <div className="w-full h-[100vh] flex">
+      <div className="w-[20%] h-full bg-gray-200">
         <Sidebar />
       </div>
-      <div className='w-[85%] 2xl:w-[90%] h-[100vh]'>
-        <Outlet />
+      <div className="w-[80%] h-[100vh]">
+        <div className="flex items-center p-5 shadow">
+          <Breadcrumb
+            items={[
+              {
+                href: "",
+                title: <HomeOutlined />,
+              },
+              ...items,
+            ]}
+          />
+        </div>
+        <div className="p-5 overflow-auto">
+          <Outlet />
+        </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ src/routers/AppRouter.tsx
@@ -0,0 +1,77 @@
+import { Routes, Route, HashRouter } from "react-router-dom";
+import RequireAuth from "./RequireAuth";
+import MainLayout from "../layouts/mainLayout";
+import AuthPage from "../pages/AuthPage";
+import Logout from "./Logout";
+import AuthLayout from "../layouts/authLayout";
+import LoadingPage from "../pages/LoadingPage";
+import DashboardPage from "../pages/DashboardPage";
+import TreatmentPage from "../pages/TreatmentPage";
+import CalendarPage from "../pages/CalendarPage";
+import CustomerListPage from "../pages/CustomerListPage";
+import CustomerServicePage from "../pages/CustomerServicePage";
+import StaffListPage from "../pages/StaffListPage";
+import IncomePage from "../pages/IncomePage";
+import PayPage from "../pages/PayPage";
+import IncomePayPage from "../pages/IncomePayPage";
+import ServicePage from "../pages/ServicePage";
+import NewPostPage from "../pages/NewPostPage";
+import IncomingEventPage from "../pages/IncomingEventPage";
+import ServiceListPage from "../pages/ServiceListPage";
+import GoodsPage from "../pages/GoodsPage";
+
+export default function AppRouter() {
+  const protectedLayout = (
+    <RequireAuth>
+      <MainLayout />
+    </RequireAuth>
+  );
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<LoadingPage />} />
+        </Route>
+        <Route path="/home" element={protectedLayout}>
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="treatments" element={<TreatmentPage />} />
+          <Route path="calendar" element={<CalendarPage />} />
+          <Route path="service-list" element={<ServiceListPage />} />
+          <Route path="goods" element={<GoodsPage />} />
+          <Route path="customers" element={<CustomerListPage />} />
+          <Route path="customer-service" element={<CustomerServicePage />} />
+          <Route path="staffs" element={<StaffListPage />} />
+          <Route path="finance">
+            <Route path="income" element={<IncomePage />} />
+            <Route path="pay" element={<PayPage />} />
+          </Route>
+          <Route path="reports">
+            <Route path="income-pay" element={<IncomePayPage />} />
+            <Route path="services" element={<ServicePage />} />
+          </Route>
+          <Route path="posts">
+            <Route path="new-post" element={<NewPostPage />} />
+            <Route path="incoming-event" element={<IncomingEventPage />} />
+          </Route>
+        </Route>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<AuthPage />} />
+          {/* <Route path="sign-up" element={<SignUpPage />} />
+          <Route
+            path="lock"
+            element={
+              <RequireAuth>
+                <LockPage />
+              </RequireAuth>
+            }
+          /> */}
+          {/* <Route path="forgot-password" element={<ForgotPasswordPage />} /> */}
+          {/* <Route path="security-code" element={<SecurityCodePage />} /> */}
+          {/* <Route path="new-password" element={<NewPasswordPage />} /> */}
+        </Route>
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </HashRouter>
+  );
+}
